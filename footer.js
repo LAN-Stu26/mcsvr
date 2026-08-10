b621f7b4797120268a52d21eec5d8f507978435c
LAN Apps Studio <lanstudio.ser25@gmail.com>
2026-08-10 19:02:40 +0800

    NA

--- footer.js
+++ footer.js
@@ -22,6 +22,7 @@ function renderFooter() {
                     <nav class="footer-links">
                         <a href="/docs/" class="footer-link">文件與目錄</a>
                         <a href="/rules/" class="footer-link">伺服器規範</a>
+                        <a href="https://github.com/LAN-Stu26/mcsvr" class="footer-link" target="_blank">GitHub</a>
                     </nav>
                 </div>
 
